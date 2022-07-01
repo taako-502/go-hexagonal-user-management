299d212d9065a4fd8248ba44963ca665ed0844bf
taako-502 <yamamoto.takao@taako-biz.com>
2022-07-01 14:38:17 +0900

    デバッグできるよう修正

--- main_test.go
+++ main_test.go
@@ -2,8 +2,16 @@ package main
 
 import "testing"
 
-func testA(t *testing.T) {
+func Test_1_run_api(t *testing.T) {
 	var url string = "https://media-engine.jp"
+	_, err := run_api(url)
+	if err != nil {
+		t.Error(`miss`)
+	}
+}
+
+func Test_2_run_api(t *testing.T) {
+	var url string = "hogehogehoge" //存在しないURL
 	_, err := run_api(url)
 	if err != nil {
 		t.Error(`miss`)
