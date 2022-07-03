3a1a373bff203278a3fc430b7dd015404edef207
taako-502 <i.am.armorknight@gmail.com>
2022-07-03 23:03:04 +0900

    feat: PSI実行

--- README.md
+++ README.md
@@ -5,6 +5,16 @@
 go run main.go
 ```
 
+## 使い方
+### 環境
+http://localhost:8080/
+
+### API仕様
+例）
+```
+http://localhost:8080/get-psi-performance?url=https://hepere.com
+'''
+
 ## 参考文献
 APIサーバを立てるための記事は以下。<br>
 [Goで作成したAPIをDockerで動かす](https://zenn.dev/kenny/articles/b9dd668bf09efe)
--- main.go
+++ main.go
@@ -1,59 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-
 	"github.com/taako-502/go-sample-api/api"
 )
 
-type Item struct {
-	Id               string           `json:"id"`
-	CaptchaResult    string           `json:"captchaResult"`
-	LighthouseResult LighthouseResult `json:"lighthouseResult"`
-}
-
-type LighthouseResult struct {
-	Categories Categories `json:"categories"`
-}
-
-type Categories struct {
-	Performance Performance `json:"performance"`
-}
-
-type Performance struct {
-	Score float64 `json:"score"`
-}
-
-func run_api(url string) ([]byte, error) {
-	resp, err := http.Get("https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" + url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
-}
-
 func main() {
-	body, err := run_api("https://hepere.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var data Item
-	if err := json.Unmarshal(body, &data); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%v: %v\n", data.Id, data.LighthouseResult.Categories.Performance.Score)
-
 	//api
-	api.Hello()
+	api.GetPSIPerformance()
 }
