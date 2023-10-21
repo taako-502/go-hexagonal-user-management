b276270ebf34f08da668b0e4326181d9ab98a173
taako-502 <i.am.armorknight@gmail.com>
2023-10-21 16:01:27 +0300

    fix: 更新されない問題の修正

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-2xl">ユーザ管理画面</h1>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div>
           <label htmlFor="username" className="mr-[2em]">
             ユーザ名:
