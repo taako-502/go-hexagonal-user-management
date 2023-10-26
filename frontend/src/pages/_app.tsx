165edad14c1f7ed49261604a06bc66b4e7bc5bf6
taako-502 <i.am.armorknight@gmail.com>
2023-10-26 14:03:26 +0300

    fix: カスタムフックでaxiosを注入できるようにする

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import './globals.scss'
+import './../styles/globals.scss'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
