1b73ba2bf06f4763f964065c3d331b814733e3ff
taako-502 <i.am.armorknight@gmail.com>
2023-10-26 23:52:19 +0300

    feat: データベースを更新できる機能を追加

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,23 @@
 import useAxios from '@/hooks/useAxios'
 import { User } from '@/type/user.type'
-import { useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 
 export default function Home() {
-  const [users, setUsers] = useState<User[]>([])
+  const [newUsers, setNewUsers] = useState<User[]>([])
+  const [editUsers, setEditUsers] = useState<{
+    [key: number]: { username: string; email: string }
+  }>({})
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [error, setError] = useState('')
+  const [editMode, setEditMode] = useState<Map<number, boolean>>(new Map())
   const axiosInstance = useAxios()
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         const res = await axiosInstance.get('/user')
-        setUsers(res.data)
+        setNewUsers(res.data)
       } catch (error) {
         console.error(error)
         if (error instanceof Error) {
@@ -29,6 +33,27 @@ export default function Home() {
     const body: User = { username, email }
     try {
       await axiosInstance.post('/user', body)
+      setUsername('')
+      setEmail('')
+      setError('')
+      // 一旦画面をリロードする
+      window.location.reload()
+    } catch (error) {
+      console.error(error)
+      if (error instanceof Error) {
+        console.error(error.message)
+        setError(error.message)
+      }
+    }
+  }
+
+  const update = async (id: number) => {
+    const body: User = {
+      username: editUsers[id].username || '',
+      email: editUsers[id].email || '',
+    }
+    try {
+      await axiosInstance.patch(`/user/${id}`, body)
       setUsername('')
       setEmail('')
       setError('')
@@ -61,6 +86,17 @@ export default function Home() {
     event.preventDefault()
   }
 
+  const toggleEditMode = (id: number) => {
+    setEditMode((prev) => {
+      const newEditMode = new Map(prev)
+      newEditMode.set(id, !newEditMode.get(id))
+      return newEditMode
+    })
+  }
+
+  // 1つでも編集モードが有効な場合にtrueとなる
+  const someEditModeIsActive = Array.from(editMode.values()).some(Boolean)
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-2xl">ユーザ管理画面</h1>
@@ -75,6 +111,7 @@ export default function Home() {
             className="border bg-gray-600 border-gray-400 rounded-md p-2 mt-[8px]"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            disabled={someEditModeIsActive}
           />
         </div>
         <div>
@@ -87,6 +124,7 @@ export default function Home() {
             className="border bg-gray-600 border-gray-400 rounded-md p-2 mt-[8px]"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            disabled={someEditModeIsActive}
           />
         </div>
         <div className="text-red-500">{error}</div>
@@ -98,10 +136,70 @@ export default function Home() {
         </button>
       </form>
       <ul>
-        {users.map((user) => (
+        {newUsers.map((user) => (
           <li key={user.id}>
             <pre className="mt-1">
-              Name: {user.username}&#009;&#009;Email: {user.email}
+              {editMode.get(user.id as number) ? (
+                <span>
+                  <label htmlFor="edit_username" className="mr-[2em]">
+                    Name:
+                  </label>
+                  <input
+                    id="edit_username"
+                    type="text"
+                    className="border bg-gray-600 border-gray-400 rounded-md p-2 mt-[8px]"
+                    value={editUsers[user.id as number]?.username || ''}
+                    onChange={(e) =>
+                      setEditUsers({
+                        ...editUsers,
+                        [user.id as number]: {
+                          ...editUsers[user.id as number],
+                          username: e.target.value,
+                        },
+                      })
+                    }
+                  />
+                  <label htmlFor="edit_email" className="mr-[2em]">
+                    Email:
+                  </label>
+                  <input
+                    id="edit_email"
+                    type="text"
+                    className="border bg-gray-600 border-gray-400 rounded-md p-2 mt-[8px]"
+                    value={editUsers[user.id as number]?.email || ''}
+                    onChange={(e) =>
+                      setEditUsers({
+                        ...editUsers,
+                        [user.id as number]: {
+                          ...editUsers[user.id as number],
+                          email: e.target.value,
+                        },
+                      })
+                    }
+                  />
+                </span>
+              ) : (
+                <Fragment>
+                  Name: {user.username}&#009;&#009;Email: {user.email}
+                </Fragment>
+              )}
+
+              <button
+                className="bg-gray-500 rounded-md px-1 py-[2px] ml-[1em]"
+                onClick={() => toggleEditMode(user.id as number)}
+              >
+                {editMode.get(user.id as number) ? 'Cancel' : 'Edit'}
+              </button>
+              {editMode.get(user.id as number) && (
+                <button
+                  className="bg-gray-500 rounded-md px-1 py-[2px] ml-[1em]"
+                  onClick={() => {
+                    update(user.id as number)
+                  }}
+                >
+                  Update
+                </button>
+              )}
               <button
                 className="bg-gray-500 rounded-md px-1 py-[2px] ml-[1em]"
                 onClick={() => {
