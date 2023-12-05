36038453541cfa9d33d4010392cf3ebde89910fc
taako-502 <i.am.armorknight@gmail.com>
2023-12-05 18:30:43 +0200

    refactor: コンポネント分割

---
+++ frontend/src/components/UserList.tsx
@@ -0,0 +1,161 @@
+import useRepository from '@/hooks/useRepository'
+import { User } from '@/type/user.type'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import DeleteButton from './DeleteButton'
+import EmailEditInput from './EmaiEditlInput'
+import NameEditInput from './NameEditlInput'
+import UpdateButton from './UpdateButton'
+
+type Props = {
+  clear: () => void
+  users: { [key: number]: User }
+  setUsers: React.Dispatch<React.SetStateAction<{ [key: number]: User }>>
+  editUsers: { [key: number]: User }
+  setEditUsers: React.Dispatch<React.SetStateAction<{ [key: number]: User }>>
+  setError: React.Dispatch<React.SetStateAction<string>>
+}
+
+const UserList: React.FC<Props> = ({
+  clear,
+  users,
+  setUsers,
+  editUsers,
+  setEditUsers,
+  setError,
+}) => {
+  const [editMode, setEditMode] = useState<Map<number, boolean>>(new Map())
+  const repository = useRepository()
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const users = await repository.user.findAll()
+        const map: { [key: number]: User } = users.reduce(
+          (acc, user) => ({
+            ...acc,
+            [user.id as number]: user,
+          }),
+          {},
+        )
+        setUsers(map)
+        setEditUsers(map)
+      } catch (error) {
+        console.error(error)
+        if (axios.isAxiosError(error) && error.response) {
+          // 404エラー以外はエラー出力する
+          if (error.response.status !== 404) {
+            console.error(error.message)
+            setError(error.message)
+          }
+        }
+      }
+    }
+    fetchUsers()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const update = async (id: number) => {
+    const user: User = {
+      id,
+      username: editUsers[id].username || '',
+      email: editUsers[id].email || '',
+    }
+    try {
+      await repository.user.update(user)
+      clear()
+      toggleEditMode(id)
+      setUsers((prev) => ({
+        ...prev,
+        [id]: {
+          ...prev[id],
+          username: user.username,
+          email: user.email,
+        },
+      }))
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        setError(error.response.data.message)
+      }
+      console.error(error)
+    }
+  }
+
+  const userDelete = async (id: number) => {
+    try {
+      await repository.user.delete(id)
+      toggleEditMode(id)
+      setUsers((prev) => {
+        const newUsers = { ...prev }
+        delete newUsers[id]
+        return newUsers
+      })
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        setError(error.response.data.message)
+      }
+      console.error(error)
+    }
+  }
+
+  const toggleEditMode = (id: number) => {
+    setEditMode((prev) => {
+      const newEditMode = new Map(prev)
+      newEditMode.set(id, !newEditMode.get(id))
+      return newEditMode
+    })
+  }
+
+  return (
+    <ul>
+      {Object.keys(users)
+        .map((key) => parseInt(key))
+        .sort((a, b) => a - b)
+        .map((key) => (
+          <li key={key}>
+            <pre className="mt-1">
+              {editMode.get(users[key].id as number) ? (
+                <span>
+                  <NameEditInput
+                    userId={users[key].id as number}
+                    editUsers={editUsers}
+                    setEditUsers={setEditUsers}
+                  />
+                  <EmailEditInput
+                    userId={users[key].id as number}
+                    editUsers={editUsers}
+                    setEditUsers={setEditUsers}
+                  />
+                </span>
+              ) : (
+                <>
+                  Name: {users[key].username}&#009;&#009;Email:{' '}
+                  {users[key].email}
+                </>
+              )}
+
+              <button
+                className="bg-gray-500 rounded-md px-1 py-[2px] ml-[1em]"
+                onClick={() => toggleEditMode(users[key].id as number)}
+              >
+                {editMode.get(users[key].id as number) ? 'Cancel' : 'Edit'}
+              </button>
+              {editMode.get(users[key].id as number) && (
+                <UpdateButton id={users[key].id as number} onClick={update} />
+              )}
+              {editMode.get(users[key].id as number) ? (
+                <></>
+              ) : (
+                <DeleteButton
+                  id={users[key].id as number}
+                  onClick={userDelete}
+                />
+              )}
+            </pre>
+          </li>
+        ))}
+    </ul>
+  )
+}
+
+export default UserList
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,110 +1,40 @@
-import DeleteButton from '@/components/DeleteButton'
-import EmailEditInput from '@/components/EmaiEditlInput'
 import EmailInput from '@/components/EmailInput'
-import NameEditInput from '@/components/NameEditlInput'
 import NameInput from '@/components/NameInput'
 import SaveButton from '@/components/SaveButton'
-import UpdateButton from '@/components/UpdateButton'
+import UserList from '@/components/UserList'
 import useRepository from '@/hooks/useRepository'
 import { User } from '@/type/user.type'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export default function Home() {
-  const [newUsers, setNewUsers] = useState<{ [key: number]: User }>({})
+  const [users, setUsers] = useState<{ [key: number]: User }>({})
   const [editUsers, setEditUsers] = useState<{ [key: number]: User }>({})
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [error, setError] = useState('')
-  const [editMode, setEditMode] = useState<Map<number, boolean>>(new Map())
   const repository = useRepository()
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const users = await repository.user.findAll()
-        const map: { [key: number]: User } = users.reduce(
-          (acc, user) => ({
-            ...acc,
-            [user.id as number]: user,
-          }),
-          {},
-        )
-        setNewUsers(map)
-        setEditUsers(map)
-      } catch (error) {
-        console.error(error)
-        if (axios.isAxiosError(error) && error.response) {
-          // 404エラー以外はコンソールに出力する
-          if (error.response.status !== 404) {
-            console.error(error.message)
-            setError(error.message)
-          }
-        }
-      }
-    }
-    fetchUsers()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   const newUser = async () => {
     const body: User = { username, email }
     try {
       const user = await repository.user.create(body)
-      setUsername('')
-      setEmail('')
-      setError('')
-      setNewUsers((prev) => ({
-        ...prev,
-        [Object.keys(prev).length]: user,
-      }))
-    } catch (error) {
-      if (axios.isAxiosError(error) && error.response) {
-        console.error(error.message)
-        setError(error.response.data.message)
-      }
-    }
-  }
+      clear()
 
-  const update = async (id: number) => {
-    const user: User = {
-      id,
-      username: editUsers[id].username || '',
-      email: editUsers[id].email || '',
-    }
-    try {
-      await repository.user.update(user)
-      setUsername('')
-      setEmail('')
-      setError('')
-      toggleEditMode(id)
-      setNewUsers((prev) => ({
-        ...prev,
-        [id]: {
-          ...prev[id],
-          username: user.username,
-          email: user.email,
+      // 新規ユーザを追加
+      const addNewUser = (
+        prevState: {
+          [key: number]: User
         },
-      }))
-    } catch (error) {
-      if (axios.isAxiosError(error) && error.response) {
-        console.error(error.message)
-        setError(error.response.data.message)
-      }
-    }
-  }
-
-  const userDelete = async (id: number) => {
-    try {
-      await repository.user.delete(id)
-      toggleEditMode(id)
-      setNewUsers((prev) => {
-        const newUsers = { ...prev }
-        delete newUsers[id]
-        return newUsers
+        newUser: User,
+      ) => ({
+        ...prevState,
+        [newUser.id as number]: newUser,
       })
+
+      setUsers((prev) => addNewUser(prev, user))
+      setEditUsers((prev) => addNewUser(prev, user))
     } catch (error) {
-      console.error(error)
       if (axios.isAxiosError(error) && error.response) {
         console.error(error.message)
         setError(error.response.data.message)
@@ -116,12 +46,10 @@ export default function Home() {
     event.preventDefault()
   }
 
-  const toggleEditMode = (id: number) => {
-    setEditMode((prev) => {
-      const newEditMode = new Map(prev)
-      newEditMode.set(id, !newEditMode.get(id))
-      return newEditMode
-    })
+  const clear = () => {
+    setUsername('')
+    setEmail('')
+    setError('')
   }
 
   return (
@@ -137,57 +65,14 @@ export default function Home() {
         <div className="text-red-500">{error}</div>
         <SaveButton onClick={newUser} />
       </form>
-      <ul>
-        {Object.keys(newUsers)
-          .map((key) => parseInt(key))
-          .sort((a, b) => a - b)
-          .map((key) => (
-            <li key={key}>
-              <pre className="mt-1">
-                {editMode.get(newUsers[key].id as number) ? (
-                  <span>
-                    <NameEditInput
-                      userId={newUsers[key].id as number}
-                      editUsers={editUsers}
-                      setEditUsers={setEditUsers}
-                    />
-                    <EmailEditInput
-                      userId={newUsers[key].id as number}
-                      editUsers={editUsers}
-                      setEditUsers={setEditUsers}
-                    />
-                  </span>
-                ) : (
-                  <>
-                    Name: {newUsers[key].username}&#009;&#009;Email:{' '}
-                    {newUsers[key].email}
-                  </>
-                )}
-
-                <button
-                  className="bg-gray-500 rounded-md px-1 py-[2px] ml-[1em]"
-                  onClick={() => toggleEditMode(newUsers[key].id as number)}
-                >
-                  {editMode.get(newUsers[key].id as number) ? 'Cancel' : 'Edit'}
-                </button>
-                {editMode.get(newUsers[key].id as number) && (
-                  <UpdateButton
-                    id={newUsers[key].id as number}
-                    onClick={update}
-                  />
-                )}
-                {editMode.get(newUsers[key].id as number) ? (
-                  <></>
-                ) : (
-                  <DeleteButton
-                    id={newUsers[key].id as number}
-                    onClick={userDelete}
-                  />
-                )}
-              </pre>
-            </li>
-          ))}
-      </ul>
+      <UserList
+        clear={clear}
+        users={users}
+        setUsers={setUsers}
+        editUsers={editUsers}
+        setEditUsers={setEditUsers}
+        setError={setError}
+      />
     </main>
   )
 }
